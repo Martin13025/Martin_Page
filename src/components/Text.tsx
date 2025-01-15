fedb90696cfdc23ecde81048b25a35c13879bb32
Martin <115161917+Martin13025@users.noreply.github.com>
2025-01-15 14:37:41 -0300

    Update Text.tsx

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -4,7 +4,7 @@ export const Text = () => {
   return (
     <div className="transparent">
       <h2>Martin Daniels</h2>
-      <h3>React Developer</h3>
+      <h3>React Developer / SEO Specialist</h3>
       <ul>
         <li>[HTML]</li>
         <li>[CSS]</li>
@@ -20,6 +20,8 @@ export const Text = () => {
         <li>[GitHub]</li>
         <li>[Docker]</li>
         <li>[Lighthouse]</li>
+        <li>[SEO Optimization]</li>
+        <li>[Google Analytics]</li>
       </ul>
     </div>
   );
